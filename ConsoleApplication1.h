e5bf4826fd07f7974391ac0bc578d100cfebe09a
f1yToMoon <90560141+f1yToMoon@users.noreply.github.com>
2022-11-28 18:24:33 +0300

    Update ConsoleApplication1.h

--- ConsoleApplication1.h
+++ ConsoleApplication1.h
@@ -1,36 +1,31 @@
 #include <iostream>
 #include <unordered_map>
+#include <iostream>
+#include <map>
 using namespace std;
-//int k = 0;
 int Kanon(vector<vector<string>> cont) {
 	cout << "start Kanon\n";
-	int k;
+	int k = 0;
 	string s, ss;
-	unordered_map<string, int> mp;
-	for (int i = 0; i < 10; ++i) {
-		s += cont[1][i];
-	}
-	mp.insert(pair<std::string, int>(s, 1));
-	for (int i = 1; i < 50000; ++i) {
-		if (i % 100 == 0) {
-			cout << i << "\n";
-		}
+	unordered_map<int, size_t> counts;
+	map<string, int> mp;
+
+	for (int i = 0; i < 50000; ++i) {
 		for (int j = 0; j < 10; ++j) {
 			ss += cont[i][j];
 		}
-		const char* c1 = ss.c_str();
-		for (auto& e : mp) {
-			const char* c2 = e.first.c_str();
-			if (strcmp(c1, c2) == 0) {
-				e.second++;
-			}
-			else {
-				mp.insert(pair<std::string, int>(ss, 1));
-			}
-		}
+		mp.insert(pair<string, int>(ss, 1));
 	}
-	k = mp.begin()->second;
+
+	for (const auto& kvp : mp) {
+		counts[kvp.second]++;
+	}
+
+	for (const auto& kvp : counts) {
+		if (kvp.second > 0)
+			std::cout << kvp.first << "->" << kvp.second << '\n';
+	}
+
 	cout << "finish Kanon\n";
 	return k;
 }
-
